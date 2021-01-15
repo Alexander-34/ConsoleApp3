48b5f6d4cfc449b0bb3973c648467f2112be5ce1
User <User@DEXY8>
2021-01-15 19:47:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = new string[3];
+            str1[0] = new string(new char[] { 'c', 'z', '1' });
+            str1[1] = new string(new char[] { 'c', 'z', '1' });
+            str1[2] = new string(new char[] { 'c', 'z', '1' });
+        }
+    }
+}
